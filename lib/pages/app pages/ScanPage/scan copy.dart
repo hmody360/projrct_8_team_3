efedf6fda979d6254cf0a309a09728dd3ae7f7c2
Saod5557 <124809082+Saod5557@users.noreply.github.com>
2024-03-24 03:12:32 +0300

    ScanPage

---
+++ lib/pages/app pages/ScanPage/scan copy.dart
@@ -0,0 +1,121 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+import 'package:project_8_team3/helper/colors.dart';
+import 'package:project_8_team3/helper/extintion.dart';
+import 'package:project_8_team3/helper/sized.dart';
+import 'package:project_8_team3/widgets/custom_widget.dart';
+
+
+
+class ScanPage extends StatefulWidget {
+  const ScanPage({Key? key}) : super(key: key);
+
+  @override
+  State<ScanPage> createState() => _ScanPageState();
+}
+
+class _ScanPageState extends State<ScanPage> {
+  String _scanBarcodeResult = '';
+
+  Future<void> startBarcodeScanStream() async {
+    FlutterBarcodeScanner.getBarcodeStreamReceiver(
+      '#ff6666',
+      'إلغاء',
+      true,
+      ScanMode.BARCODE,
+    )!
+        .listen((barcode) => debugPrint(barcode));
+  }
+
+  Future<void> scanBarcodeNormal() async {
+    String barcodeScanRes;
+    try {
+      barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
+        '#ff6666',
+        'إلغاء المسح',
+        true,
+        ScanMode.BARCODE,
+      );
+      debugPrint(barcodeScanRes);
+    } on PlatformException {
+      barcodeScanRes = 'Failed to get platform version';
+    }
+    if (!mounted) return;
+    setState(() {
+      _scanBarcodeResult = barcodeScanRes;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: whiteColor,
+      appBar: AppBar(
+       backgroundColor: transparent,
+        leadingWidth: 80,
+        leading: const CustomAppBar(),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/scaning.png', // يجب تغيير 'assets/scan_icon.png' بمسار الصورة الخاص بك
+              width: context.getWidth() + 100,
+              height: 300,
+            ),
+            Center(
+                child: Row(children: [
+              gapWe40,
+              gapWe40,
+              gapWe10,
+              gapWe10,
+              Text(
+                "    استخدم هاتفك الذكي      ",
+                style: TextStyle(fontWeight: FontWeight.w600, fontSize: 18),
+              )
+            ])),
+            Center(
+                child: Row(children: [
+              gapWe40,
+              gapWe20,
+              gapWe5,
+              gapWe10,
+              Text(
+                "    لفحص الباركود بسهولة وسرعة      ",
+                style: TextStyle(fontWeight: FontWeight.w600, fontSize: 18),
+              )
+            ])),
+            
+            SizedBox(height: 10),
+            SizedBox(
+              width: context.getWidth() - 55,
+              child: ElevatedButton(
+                 style: ElevatedButton.styleFrom(
+              shadowColor: greenText,
+              elevation: 2,
+              minimumSize: const Size(double.infinity, 60),
+              backgroundColor: whiteColor,
+              shape: RoundedRectangleBorder(
+                  side: BorderSide(color: textfieldGreenColor ?? transparent),
+                  borderRadius: const BorderRadius.all(Radius.circular(10)))),
+                onPressed: startBarcodeScanStream,
+                child: Text(
+                  " اضغط هنا " ,
+                  style: TextStyle( color: textgreyColor, fontWeight: FontWeight.w900, fontSize: 20),
+                ),
+               
+              ),
+            ),
+           
+           
+
+          ],
+        ),
+      ),
+    );
+  }
+}
